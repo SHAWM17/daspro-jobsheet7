42fcc81d92fba2407d948376b89f4a3a91076f9c
sayas44 <saumiyahya17@gmail.com>
2025-10-23 12:38:38 +0800

    feature: menambahkan penghitung jumlah yang lulus

--- SiAkadFor27.java
+++ SiAkadFor27.java
@@ -3,6 +3,7 @@ public class SiAkadFor27 {
     public static void main (String[] args) {
         Scanner sc = new Scanner(System.in);
         double nilai, tertinggi = 0, terendah = 100;
+        int lulus = 0, tidakLulus = 0;
         for (int i = 1; i <= 10; i++){
             System.out.print("Masukkan nilai siswa ke-" + i + ": ");
             nilai = sc.nextDouble();
@@ -11,9 +12,16 @@ public class SiAkadFor27 {
             } else if (nilai < terendah){
                 terendah = nilai;
             }
+            if (nilai >= 60) {
+                lulus += 1;
+            } else {
+                tidakLulus += 1;
+            }
         }
         System.out.println("Nilai tertinggi saat ini: " + tertinggi);
         System.out.println("Nilai terendah saat ini: " + terendah);
+        System.out.println("Jumlah siswa yang lulus: " + lulus);
+        System.out.println("Jumlah siswa yang tidak lulus: " + tidakLulus);
         sc.close();
         }
 }
